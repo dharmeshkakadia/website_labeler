8480f0ef2064960ab15495dca26ef23304095318
Dharmesh Kakadia <dharmesh.kakadia@gmail.com>
2021-04-06 13:09:06 -0700

    handle reset

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import csv
-import streamlit.components.v1 as components
 
 labels = ["safe","unsafe","social","educational"]
 
@@ -19,32 +18,32 @@ def insert_button(columns,labels,i,label_buttons):
     label_buttons.append(b)
 
 with open("counter", "r") as f:
-  count = f.readline()  # starts as a string
-  count = 0 if count == "" else int(count) 
+  count = int(f.readline())
 
-websites = []
-label_buttons = []
+websites,label_buttons = [],[]
 
 with open('websites.csv', newline='') as csvfile:
   for temp in csv.reader(csvfile):
     websites.append(temp[0])
   websites.append("DONE")
 
-w = websites[count]
-st.markdown(f'## [{w}]({w})')
+if count<len(websites):
+  w = websites[count]
+  if w=="DONE":
+      st.balloons()
+      if st.button("Reset"):
+        update_count(1) 
+  else:    
+    st.markdown(f'## [{w}]({w})')
+    columns = st.beta_columns(len(labels))
 
-columns = st.beta_columns(len(labels))
+    for i,l in enumerate(labels):
+        insert_button(columns,labels,i,label_buttons)
 
-for i,l in enumerate(labels):
-    insert_button(columns,labels,i,label_buttons)
+    notes=st.text_input("notes")
 
-notes=st.text_input("notes")
-
-for i,b in enumerate(label_buttons):
-  if b:
-    write_label(websites[count-1],labels[i],notes,count-1)
-    count += 1
-    update_count(count)
-
-if w=="DONE":
-    st.balloons()
+    for i,b in enumerate(label_buttons):
+      if b:
+        write_label(websites[count-1],labels[i],notes,count-1)
+        count += 1
+        update_count(count)
